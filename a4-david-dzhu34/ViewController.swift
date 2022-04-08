469393544e7d7afd6fc0b4e2baacfdadc1fdcc9a
David Zhu <dzhu34@myseneca.ca>
2022-04-07 22:58:22 -0400

    Initial Commit

---
+++ a4-david-dzhu34/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  a4-david-dzhu34
+//
+//  Created by showtimez on 2022-04-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
